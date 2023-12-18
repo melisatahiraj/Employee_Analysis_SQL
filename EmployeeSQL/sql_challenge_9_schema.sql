c4cf3f59c2c2b9bf4fbb8462ce629df0086f40fe
melisatahiraj <melisatahiraj00@gmail.com>
2023-12-17 17:58:15 -0800

    added files

---
+++ EmployeeSQL/sql_challenge_9_schema.sql
@@ -0,0 +1,57 @@
+CREATE TABLE departments (
+    dept_no VARCHAR(10) NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+SELECT * FROM departments;
+
+
+CREATE TABLE titles (
+    title_id VARCHAR(10) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+	PRIMARY KEY (title_id)
+);
+SELECT * FROM titles;
+
+
+CREATE TABLE employees (
+    emp_no INTEGER NOT NULL,
+    emp_title_id VARCHAR(10) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+SELECT * FROM employees;
+
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+SELECT * FROM dept_emp;
+
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(10) NOT NULL,
+    emp_no INTEGER NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+SELECT * FROM dept_manager;
+
+
+CREATE TABLE salaries (
+    emp_no INTEGER NOT NULL,
+    salary INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no)
+);
+SELECT * FROM salaries;
